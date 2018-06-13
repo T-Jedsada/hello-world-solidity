488326ffcd2afbbe4a757e4c8574162bd436a5d5
Jedsada Tiwongvorakul <jt@20scoops.net>
2018-06-13 12:14:49 +0700

    add example code

--- hello-world.sol
+++ hello-world.sol
@@ -3,6 +3,15 @@ pragma solidity ^0.4.16;
 contract HelloWorld {
  
     uint256 counter = 5;
+    string myName;
+
+    function constuctor () public {
+        myName = "Hello World Solidity!";
+    }
+
+    function speak () public view returns (string) {
+        return myName;
+    }
 
     function add() public {
         counter++;
