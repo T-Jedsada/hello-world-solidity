49682235dd0ceaf554682b9df37251256393b994
Jedsada Tiwongvorakul <jt@20scoops.net>
2018-06-13 11:47:10 +0700

    first commit

---
+++ hello-world.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.16;
+
+contract HelloWorld {
+ 
+    uint256 counter = 5;
+
+    function add() public {
+        counter++;
+    }
+ 
+    function subtract() public {
+        counter--;
+    }
+
+    function getCounter() public view returns (uint256) {
+        return counter;
+    }
+}
